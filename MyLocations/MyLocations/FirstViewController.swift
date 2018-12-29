d237138e0b8ad2630ce4fccb1d95aa066cbc6411
Pedro Henrique <pedrolim@br.ibm.com>
2018-12-29 14:54:24 -0200

    Creates the MyLocation project.

---
+++ MyLocations/MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Pedro Henrique Pereira Lima on 29/12/18.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MyLocations/MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Pedro Henrique Pereira Lima on 29/12/18.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
